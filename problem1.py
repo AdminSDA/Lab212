305f5ce59802a6e323138d5df54c138f359f1339
GabrielCirstea <56952518+GabrielCirstea@users.noreply.github.com>
2019-11-17 17:23:31 +0200

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,13 +1,13 @@
 from problem import Problem
 import random
 import string
- 
- 
+
+
 def random_string_generator(str_size, allowed_chars):
     return ''.join(random.choice(allowed_chars) for x in range(str_size))
- 
- 
-class Problem1(Problem):
+
+
+class Tema1(Problem):
     def __init__(self):
         chars = string.ascii_letters
         size1 = random.randrange(1, 10)
@@ -31,33 +31,33 @@ class Problem1(Problem):
         statement += '\n3 -> ' + data3
         data = data1 + ' ' + data2 + ' ' + data3
         super().__init__(statement, data)
- 
+
     def solve(self):
         # implementare stiva
         class Stack:
             def __init__(self):
                 self.items = []
                 self.v = []
- 
+
             def isEmpty(self):
                 return self.items == []
- 
+
             def push(self, item):
                 self.items.append(item)
- 
+
             def pop(self):
                 return self.items.pop()
- 
+
             def peek(self):
                 return self.items[len(self.items) - 1]
- 
+
             def size(self):
                 return len(self.items)
- 
+
             def show(self):
                 print(self.items)
- 
-            def toArray(self):
+
+            def afisare(self):
                 v = []
                 while self.isEmpty() == False:
                     x = self.pop()
@@ -67,29 +67,29 @@ class Problem1(Problem):
                     self.push(v[x])
                     x = x-1
                 self.v = list(reversed(v))
- 
+
         stack1 = Stack()
         stack2 = Stack()
- 
+
         # implementare coada
         class Node(object):
- 
+
             def __init__(self, item=None):
                 self.item = item
                 self.next = None
                 self.previous = None
- 
+
         class Queue(object):
- 
+
             def __init__(self):
                 self.size = 0
                 self.front = None
                 self.rear = None
                 self.v = []
- 
+
             def isEmpty(self):
                 return self.size == 0
- 
+
             def Push(self, x):
                 newNode = Node(x)
                 if self.isEmpty():
@@ -99,7 +99,7 @@ class Problem1(Problem):
                 newNode.previous = self.rear
                 self.rear = newNode
                 self.size += 1
- 
+
             def Pop(self):
                 item = self.front.item
                 self.front = self.front.next
@@ -107,94 +107,98 @@ class Problem1(Problem):
                 if self.isEmpty():
                     self.rear = None
                 return item
- 
-            def toArray(self):
+
+            def q_front(self):
+                if self.isEmpty():
+                    return None
+                return self.front.item
+
+            def q_rear(self):
+                if self.isEmpty():
+                    return None
+                return self.rear.item
+
+            def afisare(self):
                 v = []
                 while self.isEmpty() == False:
                     x = self.Pop()
                     v.append(x)
                 for x in v:
                     self.Push(x)
- 
+
                 self.v = v
- 
+
         queue = Queue()
         s = self.data
-        sol = "\n"
         solutie = '\nInitial propozitia este: ' + '"' + str(s) + '"'
         s = s.split(" ")
         solutie += '\nSe imparte propozitia in cuvinte intr-un vector.'
         solutie += '\nAstfel vectorul devine:  ' + str(s)
- 
-        solutie += "\nUrmeaza sa verificam daca propozitia are 3 cuvinte.\n"
+
+
         if len(s) != 3:
             print("Nu sunt 3 cuvinte\n")
- 
+
         else:
-            solutie += 'Daca indeplineste conditia introducem primul cuvant in stiva 1 astfel:\n'
- 
+            solutie += '\nIntroducem primul cuvant in stiva 1 astfel:\n'
+
             a = list(s[0])
-            solutie += 'Se imparte primul cuvant in litere (ex. afara) si la fiecare pas:\n'
+
             for x in a:
                 stack1.push(x)
-                sol += "'" + str(x) + "'" + " "
-                solutie += '\n\t\t\t\tSe foloseste doar operatia "x" si se adauga litera: ' + str(x)
-                stack1.toArray()
+
+                stack1.afisare()
                 solutie += '\n\t\t\t\tStiva 1: ' + ' ' + str(stack1.v) + '\n'
- 
+
             a = list(s[2])
             solutie += '\nVrem sa introducem in stiva 1 cel de-al treilea cuvant (pe litere), apoi il introducem in coada, apoi in stiva 2.'
-            solutie += "\nSe imparte al treilea cuvant in litere (ex. soare) si la fiecare pas:\n"
- 
+
+
             for x in a:
                 stack1.push(x)
-                sol += "'" + str(x) + "'" + " "
-                solutie += '\n\t\t\t\tSe foloseste operatia "x" si se adauga in stiva 1 litera:' + str(x)
-                stack1.toArray()
+
+                stack1.afisare()
                 solutie += '\n\t\t\t\tStiva 1 : ' + ' ' + str(stack1.v)
-                queue.Push(stack1.pop())
-                sol += "1" + " "
-                solutie += '\n\t\t\t\tSe foloseste operatia 1 si se scoate din stiva 1 litera ' + "'" + str(
-                    x) + "'" + ' apoi se introduce in coada\n'
-                stack1.toArray()
-                queue.toArray()
-                solutie += '\t\t\t\tStiva 1: ' + str(stack1.v)
-                solutie+= '\n\t\t\t\tCoada: ' + str(queue.v)
-                stack2.push(queue.Pop())
-                sol += "2" + " "
-                solutie += '\n\t\t\t\tSe foloseste operatia 2 si se scoate din coada litera ' + "'" + str(
-                    x) + "'" + ' apoi se introduce in stiva 2\n'
-                stack2.toArray()
-                queue.toArray()
-                solutie += '\t\t\t\tCoada: ' + str(queue.v)
+                var = stack1.pop()
+                var1 = '\u0336' + var
+                stack1.push(var1)
+                queue.Push(var1)
+                stack2.push(var)
+
+
+                stack2.afisare()
+                queue.afisare()
+                solutie += '\n\t\t\t\tCoada: ' + str(queue.v)
                 solutie += '\n\t\t\t\tStiva 2: ' + str(stack2.v) + '\n'
- 
- 
- 
+
+
+
             a = list(s[1])
             solutie += '\n\nVrem sa introducem in stiva 1 cel de-al doilea cuvant (pe litere), apoi il introducem in coada:\n'
-            solutie += 'Se desparte al doilea cuvant in litere si la fiecare pas:\n'
+
             for x in a:
                 stack1.push(x)
-                sol += "'" + str(x) + "'" + " "
-                solutie += '\n\t\t\t\tSe foloseste operatia "x" si se adauga in stiva 1 litera: ' + str(x)
-                stack1.toArray()
+
+                stack1.afisare()
                 solutie += '\n\t\t\t\tStiva 1: ' + str(stack1.v)
-                queue.Push(stack1.pop())
-                sol+= "1" + " "
-                solutie += '\n\t\t\t\tSe foloseste operatia 1 scotandu-se din stiva 1 litera ' "'" + str(
-                    x) + "'" + ' apoi se adauga in coada'
-                queue.toArray()
-                stack1.toArray()
+                var = stack1.pop()
+                var1 = '\u0336' + var
+                stack1.push(var1)
+                queue.Push(var)
+
+
+                queue.afisare()
+                stack1.afisare()
                 solutie += '\n\t\t\t\tStiva 1: ' + str(stack1.v)
                 solutie += '\n\t\t\t\tCoada: ' + str(queue.v) + '\n'
             solutie += 'La final:\n'
- 
-            stack1.toArray()
+
+            stack1.afisare()
             solutie += "Elementele stivei 1 sunt:\n" + str(stack1.v)
-            queue.toArray()
+            queue.afisare()
             solutie += "\nElementele cozii sunt:\n" + str(queue.v)
-            stack2.toArray()
+            stack2.afisare()
             solutie += "\nElementele stivei 2 sunt:\n" + str(stack2.v)
-            solutie += "\nSecventa de operatii este: " + sol
             return solutie
+
+
