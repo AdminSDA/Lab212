80eac4c53872c66cc9006377fba37e72b5e63032
BogdanUrmuzache <57146051+BogdanUrmuzache@users.noreply.github.com>
2019-11-17 14:20:05 +0200

    Update problem9.py

--- problem9.py
+++ problem9.py
@@ -4,17 +4,48 @@ import random
 
 class Problem9(Problem):
     def __init__(self):
-        data = random.sample(range(100), random.randint(5, 9))
+        pasi_selection = random.randint(2, 4)
+        pasi_insertion = random.randint(2, 4)
+        data = random.sample(range(100), random.randint(6, 10))
+        x = random.randint(0,2)
+        y = random.randint(0,2)
+
+        pivot = -1
+        if y == 0:
+            max = data[0]
+            m = 0
+            for i in range (1,len(data)):
+                if data[i] > max:
+                    max = data[i]
+                    m = i
+            data[m], data[len(data)-1] = data[len(data)-1], data[m]
+            pivot = data[len(data)-1]
+
+        if pivot == -1:
+            pivot = random.choice(data)
+
+        # 33% din cazuri ne asiguram ca prima cerinta e adevarata
+        reverse = True
+        if x == 2:
+            while reverse:
+                reverse = False
+                for i in range(0, pasi_selection):
+                    if data[i + 1] > data[i]:
+                        data[i], data[i + 1] = data[i + 1], data[i]
+                        reverse = True
+        # 33% din cazuri ne asiguram ca a doua cerinta e adevarata
+        if x == 1:
+            while reverse:
+                reverse = False
+                for i in range(0, pasi_insertion):
+                    if data[i -1] > data[i]:
+                        data[i], data[i - 1] = data[i - 1], data[i]
+                        reverse = True
 
         statement = 'Problema 9: Se primesc numerele: ' + ', '.join(map(str, data)) + '.\n'
-        statement += 'Verificati daca:\n'
-
-        # alegem pivotul si pasii de selection (maxim) si insertion
-        pivot = random.choice(data)
-        pasi_selection = random.randint(2, len(data) - 1)
-        pasi_insertion = random.randint(2, len(data) - 1)
-
+        statement += 'Raspundeti cu adevarat sau fals:\n'
         data = [data, pivot]
+
 
         data = [data, pasi_selection]
         statement += '1. Vectorul a rezultat in urma aplicarii a ' + str(pasi_selection) + ' pasi din Selection Sort (Maxim).\n'
@@ -48,105 +79,91 @@ class Problem9(Problem):
         # verificare selection sort (maxim)
         ok = 1
         n = pasi_selection
-        solution += 'Trebuie sa aflam daca este adevarat sau fals ca vectorul ' + str(data) + ' rezulta din ' + str(n) + ' pasi de Selection Sort (Maxim) : \n'
-        solution += 'Pentru asta trebuie sa verificam daca primele ' + str(n) + ' elemente sunt in ordine descrescatoare \n'
+        solution += '1. A/F ca ' + str(data) + ' rezulta din ' + str(n) + ' pasi de Selection Sort (Maxim) : \n'
         for i in range(0, n):
             if data2[i + 1] > data2[i]:
-                solution += str(data2[i + 1]) + ' > ' + str(data2[i]) + ' => Afirmatia este falsa \n'
+                solution += str(data2[i + 1]) + ' > ' + str(data2[i]) + ' => (FALS) \n\n'
                 ok = 0
                 break
             else:
                 solution += str(data2[i + 1]) + ' < ' + str(data2[i]) + '\n'
         if ok == 1:
-            solution += '(ADEVARAT) Vectorul a rezultat in urma aplicarii a ' + str(n) + ' pasi din Selection Sort (Maxim)\n\n'
-        else:
-            solution += '(FALS) Vectorul nu a rezultat in urma aplicarii a ' + str(n) + ' pasi din Selection Sort (Maxim)\n\n'
+            solution += '(ADEVARAT)\n\n'
 
         # verificare insertion sort
         ok = 1
         n = pasi_insertion
-        solution += 'Trebuie sa aflam daca este adevarat sau fals ca vectorul ' + str(data) + ' rezulta din ' + str(n) + ' pasi de Insertion Sort : \n'
-        solution += 'Pentru asta trebuie sa verificam daca primele ' + str(n) + ' elemente sunt deja sortate in ordine crescatoare \n'
+        solution += ' 2. A/F ca ' + str(data) + ' rezulta din ' + str(n) + ' pasi de Insertion Sort : \n'
         for i in range(1, n):
             if data2[i] < data2[i - 1]:
                 ok = 0
-                solution += str(data2[i]) + ' < ' + str(data2[i - 1]) + ' => Afirmatia este falsa \n'
+                solution += str(data2[i]) + ' < ' + str(data2[i - 1]) + ' => (FALS) \n\n'
                 break
             else:
                 solution += str(data2[i]) + ' > ' + str(data2[i - 1]) + '\n'
         if ok == 1:
-            solution += '(ADEVARAT) Vectorul a rezultat in urma aplicarii a ' + str(n) + ' pasi din Insertion Sort \n\n'
-        else:
-            solution += '(FALS) Vectorul nu a rezultat in urma aplicarii a ' + str(n) + ' pasi din Insertion Sort \n\n'
+            solution += '(ADEVARAT) \n\n'
 
         # verificare pivot
         n = len(data2)
-        solution += 'Verificam partitionarea pentru Quicksort cu pivotul ' + str(pivot) + '. Vectorul ' + str(data2) + ' are ' + str(n) + ' elemente \n'
+        solution += '3. A/F partitionat pentru Quicksort cu pivotul ' + str(pivot) + ' \n'
         ok1 = 1
         ok2 = 1
-        solution += 'Pentru a verifica daca vectorul este partitionat in functie de un anumit pivot vom face urmatoarele: \n'
         for i in range(0, n):
             if data2[i] == pivot:
                 poz_pivot = i
-                solution += 'Identificam ca pivotul se afla pe pozitia ' + str(i + 1) + '\n'
-        solution += 'Verificam daca elementele care se afla la stanga pivotului sunt mai mici decat acesta \n'
+                solution += 'i. Pozitia pivotului = ' + str(i + 1) + '\n'
+        solution += 'ii. Vf. elementele de la stanga sunt mai mici \n'
         for i in range(0, poz_pivot):
             if data2[i] > pivot:
                 ok1 = 0
-                solution += str(data2[i]) + ' >= ' + str(pivot) + ' => Afirmatia este falsa \n'
+                solution += str(data2[i]) + ' >= ' + str(pivot) + ' => (FALS) \n\n'
                 break
             else:
                 solution += str(data2[i]) + ' <= ' + str(pivot) + '\n'
-        solution += 'Verificam daca elementele care se afla la dreapta pivotului sunt mai mari decat acesta \n'
+        solution += 'iii. Vf elementele de la dreapta mai mari\n'
         for j in range(poz_pivot + 1, n):
             if data2[j] < pivot:
                 ok2 = 0
-                solution += str(data2[j]) + ' <= ' + str(pivot) + ' => Afirmatia este falsa \n'
+                solution += str(data2[j]) + ' <= ' + str(pivot) + ' => (FALS) \n\n'
                 break
             else:
                 solution += str(data2[j]) + ' >= ' + str(pivot) + '\n'
         if ok1 == 1 and ok2 == 1:
-            solution += '(ADEVARAT) Vectorul a rezultat in urma unei partirionari folosind pivotul ' + str(pivot) + '\n\n'
-        else:
-            solution += '(FALS) Vectorul nu a rezultat in urma unei partitionari folosind pivotul ' + str(pivot) + '\n\n'
+            solution += '(ADEVARAT)\n\n'
 
         # bubble sort
-        solution += 'Vom exemplifica modalitatea de functionare a Bubble sort-ului: \n'
-        solution += 'Aflam dimensiunea vectorului ' + str(data) + ' pe care vom aplica algoritmul \n'
+        solution += '4. Exemplificam Bubble sort: \n'
         n = len(data)
-        solution += 'Vectorul nostru are ' + str(n) + ' elemente \n'
-        solution += 'Vom folosi o variabila booleana pentru a verifica daca am facut interschimbari \n'
+        solution += 'i.Vectorul are ' + str(n) + ' elemente \n'
         reverse = True
-        solution += 'Cat timp vom face interschimbari iar variabila bool va fi adevarata, algoritmul nu se termina iar vectorul nu este complet sortat \n'
+        solution += 'ii.Cat timp facem interschimbari = > nu am terminat \n'
         while reverse == True:
             reverse = False
-            solution += 'Vom parcurge vectorul \n'
             for i in range(0, n - 1):
                 if data[i + 1] < data[i]:
                     data[i], data[i + 1] = data[i + 1], data[i]
                     solution += str(data[i + 1]) + ' < ' + str(data[i]) + ' => elementele nu sunt sortate asa ca vom face o interschimbare \n'
                     reverse = True
-        solution += 'Daca nu s-a mai facut nicio interschimbare la pasul anterior, atunci vectorul este sortat si il afisam: ' + str(data) + '\n\n'
+        solution += 'iii.' + str(data) + '\n\n'
 
         # selection sort (maxim)
-        solution += 'Vom exemplifica modalitatea de functionare a Selection Sort(Maxim): \n'
-        solution += 'Aflam dimensiunea vectorului ' + str(data2) + ' pe care vom aplica algoritmul \n'
+        solution += '5.Exemplificam Selection Sort(Maxim): \n'
         n = len(data2)
-        solution += 'Vectorul nostru are ' + str(n) + ' elemente \n'
-        solution += 'Vom parcurge vectorul nesortat pentru a gasi elementul maxim si vom face o interschimbare astfel incat vom avea vectorul partitionat in 2: partea ordonata descrescator la stanga si cea neordonata la dreapta \n'
+        solution += 'i.Vectorul are ' + str(n) + ' elemente \n'
         for i in range(0, n):
             poz_max = i
             elem_max = data2[i]
-            solution += 'Selectam elementul de pe pozitia ' + str(i + 1) + ' si consideram ca elementul ' + str(elem_max)+ ' este maxim \n'
-            solution += 'Vom parcurge vectorul la dreapta elementului maxim \n'
+            solution += 'ii. poz_max = ' + str(i + 1) + ' max = ' + str(elem_max) + ' \n'
             for j in range(i, n):
                 if data2[j] > elem_max:
-                    solution += str(data2[j]) + ' > ' + str(elem_max) + ' => am gasit un element mai mare decat cel maxim in partitia neordonata a vectorului asa ca salvam elementul ca fiind noul maxim \n'
+                    solution += str(data2[j]) + ' > ' + str(elem_max) + ' => max =' + str(data2[j]) + '\n'
                     elem_max = data2[j]
                     poz_max = j
             data2[i], data2[poz_max] = data2[poz_max], data2[i]
             if i != poz_max:
-                solution += 'Interschimbam elementele ' + str(data2[i]) + ' si ' + str(data2[poz_max]) + '\n'
-        solution += 'Dupa ce terminam parcurgerea intregului vector si am terminat sortarea, il afisam: ' + str(data2) + '\n \n'
+                solution += 'iii. '
+                solution +=  str(data2[i]) + ' se interschimba cu ' + str(data2[poz_max]) + '\n'
+        solution += 'iv. ' + str(data2) + '\n \n'
 
         return solution
