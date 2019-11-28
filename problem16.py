049c700d53f3af36147b5cb4f2f82259bd482aba
GabrielCirstea <56952518+GabrielCirstea@users.noreply.github.com>
2019-11-28 15:10:35 +0200

    Add files via upload

--- problem16.py
+++ problem16.py
@@ -46,24 +46,20 @@ class Problem16(Problem):
     
         # acum mutam pivotul si toate elementele...
         print("");
+        self.interschimbari = "\nInainte de mutarea elementelor egale cu pivotul:\n";
+        self.interschimbari += str(v);
+        self.interschimbari +="\nPivootul si toate elementele egale cu pivotul sunt la sfarsitul sirului \n";
+        self.interschimbari += "Acum mutam pivotul si dublurile dupa ultimul element mai mic decat pivotul\n"
         j = i+1;
-        while(p<=right):
+        while(p<=right): #plecam de la p pana la capatul sirului
+                         # si mutam elementele egale cu pivotul acolo unde trebuie sa fie
             v[j], v[p] = v[p], v[j];
             j+=1;
             p+=1;
-            # print(v)
+            nr+=1; #inca o interschimbare
         self.nr = nr;
-        return i + 1  # pozitia pivotului
+        return i + 1  # si j capetele intervalului de elemente egale cu piv
 
-    def verificare(self, v, Ipiv):
-        for i in range(0, Ipiv):
-            if v[i] > v[Ipiv]:
-                print("aiurea")
-                return
-        for i in range(Ipiv, len(v) - 1):
-            if (v[i] < v[Ipiv]):
-                print("aiurea")
-                return
 
     def solve(self):
         solution = "<br><br>Idee de rezolvare:<br><br>"
@@ -85,16 +81,18 @@ class Problem16(Problem):
         solution += "Se parcurge sirul si se muta elementele mai mici sau egale cu pivotul inaintea "
         solution += "pivotului si cele mai mari dupa el."
         solution += "Se muta elementele la inceputul vectorului pana la pozitia i+1. <br><br>"
-        solution += "<br><br>Secventa finala este: "
 
 
 
         # print(self.data)
         v = self.data[0]
         Ipiv = v.index(self.data[1])
+        solution +="Indexul pivotului este: " + str(Ipiv) + "\n"
         self.nr = 0;
+        self.interschimbari = ""
         Ipiv = self.partition(v, 0, len(v) - 1, Ipiv)
-        self.verificare(v, Ipiv)
+        solution += self.interschimbari + '\n';
+        solution += "<br><br>Secventa finala este: "
         for i in range(len(v)):
               y =  str(v[i])
               solution += y +" "
