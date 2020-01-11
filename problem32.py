f7e04cc8fa3be6c5fbc095c376e9cdf4cd78e6db
Catalin Radu <rcatalingabriel@gmail.com>
2020-01-11 19:51:24 +0200

    New changes

--- problem32.py
+++ problem32.py
@@ -11,10 +11,16 @@ class Problem32(Problem):
         # generam un arbore binar Prim Min ABC, functia este recursiva
         self.generatePrimMinABC(data, 0, len(data) - 1)
         statement = 'Un Prim-Min ABC este un arbore cu urmatoarele proprietati:\n'
-        statement +='• radacina are valoarea minima din arbore;\n'
-        statement +='• fiecare valoare din sub-arborele st. are o valoare mai mica decat orice valoare din subarborele dr;\n'
-        statement +='• sub-arborele stang si sub-arborele drept sunt Prim-Min ABC.\n'
-        statement +='Primind un Prim-Min ABC si un numar k, decideti daca k apare in acest arbore.'
+        statement +='> radacina are valoarea minima din arbore;\n'
+        statement +='> fiecare valoare din sub-arborele st. are o valoare mai mica decat orice valoare din subarborele dr;\n'
+        statement +='> sub-arborele stang si sub-arborele drept sunt Prim-Min ABC.\n'
+        statement += f'Primim numarul {self.search} si Prim-Min ABC-ul dat de lista de copii:\n'
+        for element in data:
+            stanga = data[element[1]][0] if element[1] else None
+            dreapta = data[element[2]][0] if element[2] else None
+            
+            statement += f'\t{element[0]}: {stanga}, {dreapta}\n'
+        statement += f'Cerinta: Decideti daca acesta  apare in acest arbore.\n'
         super().__init__(statement, data)
 
     def generatePrimMinABC(self, data, start, final):    
@@ -33,28 +39,31 @@ class Problem32(Problem):
 
 
     def solve(self):
+        solution = ''
         v = self.data
-        solution = f'•Arborele generat este:\n{v}\n'
         parcurgere = v[0]
-
-        while parcurgere is not None:
-            if(parcurgere[0] == self.search):
-                solution += f'•Numarul {self.search} se afla in acest arbore\n'
+        solution += f'Parcurgem arborele incepand cu radacina pana gasim nodul cautat. De fiecare data cand ne mutam la un nod verificam daca este cel cautat. Altfel, decidem daca mergem la nodul din stanga, respectiv nodul din dreapta\n'
+        while parcurgere is not None:      
+            if parcurgere[0] is not None and parcurgere[0] == self.search:
+                solution += f'\t\tNodul curent este egal cu {self.search}\n'
+                solution += f'\nNumarul {self.search} se afla in acest arbore\n'
                 break
+            else:
+                solution += f'\t\tNodul curent nu are valoarea {self.search}\n'
+
             if (parcurgere[1] is None and parcurgere[2] is None) or (parcurgere[1] is not None and self.search < v[parcurgere[1]][0]):
-                solution += f'•Numarul {self.search} NU se afla in acest arbore\n'
+                solution += f'\nNumarul {self.search} NU se afla in acest arbore\n'
                 break
             if parcurgere[2] is not None and self.search >= v[parcurgere[2]][0]:
+                solution += f'\t> Numarul cautat {self.search} >= {v[parcurgere[2]][0]}. Ne mutam la nodul din dreapta\n'
                 parcurgere = v[parcurgere[2]]
             elif parcurgere[1] is not None:
+                solution += f'\t> Numarul cautat {self.search} >= {v[parcurgere[1]][0]}. Ne mutam la nodul din stanga\n'
                 parcurgere = v[parcurgere[1]]
             else:
-                solution += f'•Numarul {self.search} NU se afla in acest arbore\n'
-
+                solution += f'\nNumarul {self.search} NU se afla in acest arbore\n'
                 break
-        solution += f'•Mergem pe fiecare nivel al arborelui verificand daca\n'
-        solution += f' numarul {self.search} apare in acesta, accesand fiecare element al sau.\n'
-        solution += f'•Daca numarul cautat este gasit s-a incheiat programul.\n'
+                
         return solution
 
 p = Problem32()
